d6d37a2474f34a773cbe914f45705ad8fd30855a
Suraj <surajjadhav5335@gmail.com>
2023-03-28 18:38:17 +0530

    Add project files.

---
+++ NEW_EMPLOYEE/Controllers/EmpController.cs
@@ -0,0 +1,47 @@
+﻿using NEW_EMPLOYEE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NEW_EMPLOYEE.Controllers
+{
+    public class EmpController : Controller
+    {
+        // GET: Emp
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee obj)
+        {
+            using (var ctx = new EMP_DBEntities())
+            {
+                tblEmployee Emp = new tblEmployee();
+                Emp.Name = obj.Name;
+                Emp.Address = obj.Address;
+                Emp.MobileNo = obj.MobileNo;
+                Emp.Email = obj.Email;
+                Emp.JoiningDate = obj.JoiningDate;
+                Emp.Password = obj.Password;
+                ctx.tblEmployees.Add(Emp);
+                ctx.SaveChanges();
+            }
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            List<tblEmployee> Elist = new List<tblEmployee>();
+            using (var ctx = new EMP_DBEntities())
+            {
+                Elist = ctx.tblEmployees.ToList();
+                
+            }
+            return View(Elist);
+        }
+    }
+}
---
+++ NEW_EMPLOYEE/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NEW_EMPLOYEE.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string MobileNo { get; set; }
+        public string Email { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public string Password { get; set; }
+    }
+}
